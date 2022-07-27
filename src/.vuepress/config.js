5575ee9cb0a911df38b4e22103b24da41610e11a
Clemie McCartney <27568445+clemiee@users.noreply.github.com>
2022-07-27 10:55:01 +0700

    revert

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -20,7 +20,6 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
-
   /**
    * Theme configuration, here is the default theme configuration for VuePress.
    *
